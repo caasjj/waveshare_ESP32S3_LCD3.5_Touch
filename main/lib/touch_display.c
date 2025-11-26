70b31cc89e8250c8b4bf7658e114de96e331ec19
Walid Hosseini <caasjj@gmail.com>
2025-11-26 11:05:29 -0800

    Absorb click events in top layer to wake up screensaver, to prevent blind clicking on active screen widgets

--- main/lib/touch_display.c
+++ main/lib/touch_display.c
@@ -8,6 +8,18 @@
 #include "touch_display.h"
 #include "screensaver.h"
 
+esp_err_t screensleep(void)
+{
+    lv_obj_add_flag(lv_layer_top(), LV_OBJ_FLAG_CLICKABLE);
+    return bsp_display_sleep();
+}
+
+esp_err_t screenwake(void)
+{
+    lv_obj_remove_flag(lv_layer_top(), LV_OBJ_FLAG_CLICKABLE);
+    return bsp_display_wake();
+}
+
 static void global_touch_cb(lv_event_t *e)
 {
     lv_event_code_t code = lv_event_get_code(e);
@@ -26,6 +38,6 @@ void touch_display_init()
     lv_indev_t *lvgl_touch = AXS15231B_touch_init(lvgl_disp);
 
     /* Initialize screensaver */
-    screensaver_init(CONFIG_ESP32S3_SCREENSAVER_TIMEOUT, &bsp_display_sleep, &bsp_display_wake); // Set screensaver to activate after configured seconds of inactivity
+    screensaver_init(CONFIG_ESP32S3_SCREENSAVER_TIMEOUT, &screensleep, &screenwake); // Set screensaver to activate after configured seconds of inactivity
     lv_indev_add_event_cb(lvgl_touch, global_touch_cb, LV_EVENT_PRESSED, NULL);
 }
